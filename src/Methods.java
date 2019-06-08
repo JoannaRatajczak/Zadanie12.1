2a9e3323ed7f216dafe9fb613d8457ce512c8ba8
JoannaRatajczak <joanna.jr.ratajczak@gmail.com>
2019-06-09 00:25:53 +0200

    Runtime Ex

---
+++ src/Methods.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Methods {
+
+    static Person scanNewPerson() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Wprowadź dane nowej osoby: \nImię, nazwisko, wiek, pesel");
+        String fn = scanner.nextLine();
+        if (fn.length() < 2 || fn ==null) throw new NameUndefinedException();
+        String ln = scanner.nextLine();
+        if (ln.length() < 2 || ln ==null) throw new NameUndefinedException();
+        int a = scanner.nextInt();
+        if (a<0) throw new IncorrectAgeException();
+        int p = scanner.nextInt();
+        return new Person(fn, ln, a, p);
+    }
+}
